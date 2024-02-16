4f6ca7035cf867cef5e6bb47e22ee63db043bca2
testpanno <108088135+testpanno@users.noreply.github.com>
2024-02-16 18:28:48 +0700

    Update navbar.js

--- assets/js/navbar.js
+++ assets/js/navbar.js
@@ -19,12 +19,12 @@ const main_cta = document.querySelector('.main-cta')
 
 main_cta.addEventListener("mouseover", function (){
     document.querySelector(".main-cta button").style.background = 'white'
-    document.querySelector(".main-cta button img").src = '/assets/img/dark-arrow-left.svg'
+    document.querySelector(".main-cta button img").src = 'luxceiling/assets/img/dark-arrow-left.svg'
 })
 
 main_cta.addEventListener("mouseout", function (){
     document.querySelector(".main-cta button").style.background = '#141414'
-    document.querySelector(".main-cta button img").src = '/assets/img/arrow-left.svg'
+    document.querySelector(".main-cta button img").src = 'luxceiling/assets/img/arrow-left.svg'
 })
 
 
@@ -32,12 +32,12 @@ const footer_cta = document.querySelector('.footer-cta')
 
 footer_cta.addEventListener("mouseover", function (){
     document.querySelector(".footer-cta button").style.background = 'white'
-    document.querySelector(".footer-cta button img").src = '/assets/img/dark-arrow-left.svg'
+    document.querySelector(".footer-cta button img").src = 'luxceiling/assets/img/dark-arrow-left.svg'
 })
 
 footer_cta.addEventListener("mouseout", function (){
     document.querySelector(".footer-cta button").style.background = '#141414'
-    document.querySelector(".footer-cta button img").src = '/assets/img/arrow-left.svg'
+    document.querySelector(".footer-cta button img").src = 'luxceiling/assets/img/arrow-left.svg'
 })
 
 
